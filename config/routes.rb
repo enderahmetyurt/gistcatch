91f614e55dcbcfb283ea726a45d09f2fa2e6e48e
Ender Ahmet Yurt <enderyurt@gmail.com>
2018-10-13 13:38:07 +0300

    Fix destroy session path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }, skip: [:sessions, :registrations]
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   root to: "welcome#index"
 
